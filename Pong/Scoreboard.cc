370e7ac43da0865a31264dcba2945b5382cf256e
GT162 <59036640+GTOneSixTwo@users.noreply.github.com>
2021-12-17 14:18:18 -0500

    winner screen

--- Pong/Scoreboard.cc
+++ Pong/Scoreboard.cc
@@ -4,10 +4,19 @@
 
 #include "Scoreboard.h"
 
-Scoreboard::Scoreboard(AGE4Scene* scene) : AGE4Actor{scene} {}
+Scoreboard::Scoreboard(AGE4Scene *scene) : AGE4Actor{scene} {}
 
 void Scoreboard::doTick(AGE4InputAction inputAction) {
-  parent->status[1] = "                              " + std::to_string(left) +
-                      "     -     " + std::to_string(right) +
-                      "                              ";
+  parent->status[1] = "                                   " +
+                      std::to_string(left) + "     -     " +
+                      std::to_string(right);
+  if (left >= 11) {
+    parent->status[0] = "PLAYER 1 WINS!";
+    left = 0;
+    right = 0;
+  } else  if (right >= 11) {
+    parent->status[0] = "PLAYER 2 WINS!";
+    left = 0;
+    right = 0;
+  }
 }
